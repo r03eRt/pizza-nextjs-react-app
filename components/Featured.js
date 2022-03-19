8c3155f56a2d7d5ae9452fcc7a9590f2b40aa730
Roberto Morgado Luengo <robertomorgadoluengo@MacBook-Pro-de-Roberto.local>
2022-03-19 02:45:40 +0100

    Created main structure and Navbar

---
+++ components/Featured.js
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import styles from '../styles/Featured.module.css'
+
+
+export const Featured = () => {
+  return (
+    <div className={styles.container}>
+        Featured
+    </div>
+  )
+}
---
+++ components/Layout.js
@@ -0,0 +1,12 @@
+import { Footer } from "./Footer"
+import { Navbar } from "./Navbar"
+
+export const Layout = ({ children }) => {
+  return (
+    <>
+        <Navbar></Navbar>
+        { children }
+        <Footer></Footer>
+    </>
+  )
+}
